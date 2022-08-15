6ebfd0d0638d221d048c6541bc1e5695134396df
442Staysad <kerri.man@mail.ru>
2022-08-15 22:31:37 +0300

    DTO Updated, Password Change Added

--- Onion.Core/DTO/Employee/EmployeeDTO.cs
+++ Onion.Core/DTO/Employee/EmployeeDTO.cs
@@ -12,7 +12,7 @@ namespace Onion.Core.DTO.Employee
         public string WorkPhoneNumber { get; set; }
         public string WorkEmailAddress { get; set; }
 
-        public EmployeeShortDTO? EmployeeShort { get; set; }
+        public EmployeeShortDTO? PersonalData { get; set; }
         public DepartmentDTO? DepartmentDTO { get; set; }
 
     }
--- Onion.Core/DTO/Employee/PasswordEditDTO.cs
+++ Onion.Core/DTO/Employee/PasswordEditDTO.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Onion.Core.DTO.Employee
 {
-    public class EmployeeUpdateDTO
+    public class PasswordEditDTO
     {
         public string Password { get; set; }
         public EmployeeDTO EmployeeDTO { get; set; }
